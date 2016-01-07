a1e9fc7f0d82910c659f5a30a19d037e92ac219c
rockdragon <moyerock@gmail.com>
2016-01-07 18:26:48 +0800

    initial commit.

---
+++ app.scala
@@ -0,0 +1,7 @@
+package com.example
+
+object App {
+        def calc(x:Int, y:Int) : Int = {
+                x + y
+        }
+}
---
+++ main.scala
@@ -0,0 +1,7 @@
+package com.example
+
+object Main {
+        def main(args: Array[String]): Unit =  {
+                println(App.calc(10, 20))
+        }
+}
