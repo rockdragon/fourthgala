0a1fed60597cbd1fc669d688ffd5ad0ed54115f3
Locke <moyerock@gmail.com>
2016-02-11 17:35:55 +0800

    covariant & contravariant

--- src/main/scala/MixinObservers.scala
+++ src/main/scala/MixinObservers.scala
@@ -16,9 +16,7 @@ trait Subject[State] {
     observers.foreach(_.receiveUpdate(state))
 }
 
-class Button(val label: String) extends Widget {
-  def click(): Unit = updateUI()
-
+class Button(val label: String) extends Widget with Clickable {
   def updateUI(): Unit = {
     /*..*/
   }
@@ -39,11 +37,45 @@ class ButtonCountObserver extends Observer[Button] {
   def receiveUpdate(state: Button): Unit = count += 1
 }
 
+trait Clickable {
+  def click(): Unit = updateUI()
+
+  protected def updateUI(): Unit
+}
+
+trait ObservableClicks extends Clickable with Subject[Clickable] {
+  abstract override def click(): Unit = {
+    super.click()
+    notifyObservers(this)
+  }
+}
+
+class ClickCountObserver extends Observer[Clickable] {
+  var count = 0
+
+  def receiveUpdate(state: Clickable): Unit = count += 1
+}
+
+trait VetoableClicks extends Clickable {
+  val maxAllowed = 1
+  private var count = 0
+
+  abstract override def click() = {
+    if (count < maxAllowed) {
+      count += 1
+      super.click()
+    }
+  }
+}
+
 object main {
   def main(args: Array[String]): Unit = {
-    val button = new ObservableButton("Click me")
-    val bco1 = new ButtonCountObserver
-    val bco2 = new ButtonCountObserver
+    val button = new Button("Click Me!") with ObservableClicks with VetoableClicks {
+      override val maxAllowed = 2
+    }
+
+    val bco1 = new ClickCountObserver
+    val bco2 = new ClickCountObserver
 
     button addObserver bco1
     button addObserver bco2
