e6a43db519145b900476db2d3be57899435fb6b4
Locke <moyerock@gmail.com>
2016-04-10 17:20:32 +0800

    just a practise.

---
+++ src/main/scala/Exercise.scala
@@ -0,0 +1,24 @@
+package me.moye.practise
+
+import scala.collection.mutable.ArrayBuffer
+
+object main {
+  def exchange(array: Array[Int]) = {
+    val length = array.length
+    val buffer = ArrayBuffer[Int]()
+    buffer ++= array
+
+    for (i <- 0 to length / 2)
+      if (i * 2 + 1 < buffer.length) {
+        val temp = buffer(i * 2)
+        buffer(i * 2) = buffer(i * 2 + 1)
+        buffer(i * 2 + 1) = temp
+      }
+
+    buffer.toArray
+  }
+
+  def main(args: Array[String]): Unit = {
+    exchange(Array(1, 2, 3, 4, 5))
+  }
+}
