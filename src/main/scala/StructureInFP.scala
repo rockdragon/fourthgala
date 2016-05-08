4a9b2a75599ba19441c7a8460982b346701d28ca
Locke <moyerock@gmail.com>
2016-05-08 18:52:08 +0800

    List[A] in scalainfp ver.

--- src/main/scala/StructureInFP.scala
+++ src/main/scala/StructureInFP.scala
@@ -20,9 +20,41 @@ object main {
       case Cons(x, xs) => x * product(xs)
     }
 
+    def append[A](a1: List[A], a2: List[A]): List[A] =
+      a1 match {
+        case Nil => a2
+        case Cons(h, t) => Cons(h, append(t, a2))
+      }
+
+    def foreach[A](as: List[A])(f:(A) => Unit): Unit = {
+      as match {
+        case Cons(h, t) => {
+          f(h)
+          foreach(t)(f)
+        }
+        case _ =>
+      }
+    }
+
+
     def apply[A](as: A*): List[A] =
       if (as.isEmpty) Nil
       else Cons(as.head, apply(as.tail: _*))
+  }
+
+  @annotation.tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f:(A, B) => B): B = {
+    as match {
+      case Cons(h, t) => foldLeft(t, f(h, z))(f)
+      case Nil => z
+    }
+  }
+
+  def map[A, B](as: List[A], processed: List[B])(f:(A) => B): List[B] = {
+    as match {
+      case Nil => processed
+      case Cons(h, t) => map(t, List.append(processed, List(f(h))))(f)
+    }
   }
 
   def main(args: Array[String]): Unit = {
@@ -37,6 +69,10 @@ object main {
 
     println(x)
 
+    println(foldLeft(List(1 to 15: _*), 1)((a, b) => a * b))
+
+    val chars = map(List('a' to 'f': _*), Nil)(c => (c + 1).toChar)
+    List.foreach(chars)(ch => println(ch))
   }
 
 }
