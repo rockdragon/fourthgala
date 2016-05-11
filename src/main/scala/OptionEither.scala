1c1924104c13349264f95664bb980de6f993f342
Locke <moyerock@gmail.com>
2016-05-11 17:09:09 +0800

    option: map2.

---
+++ src/main/scala/OptionEither.scala
@@ -0,0 +1,16 @@
+package me.moye.practise.optionEither
+
+object main {
+
+  def map2[A, B, C](a: Option[A], b:Option[B])(f: (A, B) => C): Option[C] =
+    a flatMap(aa =>
+      b map (bb =>
+        f(aa, bb)))
+
+  def main(args: Array[String]): Unit = {
+    val a: Option[Int] = Some(3)
+    val b: Option[Int] = Some(4)
+    println(map2(a, b)((a, b) => a + b))
+  }
+
+}
