7161280e4777a0c95693bdd0f59422e78d7fccfe
Locke <moyerock@gmail.com>
2016-06-10 17:57:35 +0800

    feat:network equals

---
+++ src/main/scala/FluidInterface.scala
@@ -0,0 +1,31 @@
+package me.moye.practise.fluidInterface
+
+object Title
+object Author
+
+class Book(var title: String = "", var author: String = "") {
+  var nextArg:Any = null
+
+  def set(o: Title.type): this.type = {
+    nextArg = o
+    this
+  }
+  def set(o:Author.type): this.type = {
+    nextArg = o
+    this
+  }
+  def to(s:String): this.type = {
+    if(nextArg == Title) title = s else author = s
+    this
+  }
+
+  override def toString:String = "<" + title + "> by " + author
+  def display(): Unit = println(this)
+}
+
+object main {
+  def main(args: Array[String]): Unit = {
+    val book = new Book()
+    book set Title to "Scala for the impatient" set Author to "Cay Horstman" display
+  }
+}
---
+++ src/main/scala/NetworkMember.scala
@@ -0,0 +1,36 @@
+package me.moye.practise.networkMember
+
+import scala.collection.mutable.ArrayBuffer
+
+class Network {
+  class Member(val name: String) {
+    val contacts = members
+
+    def equals(m:Network#Member):Boolean = m.contacts == this.contacts
+  }
+
+  private val members = new ArrayBuffer[Network#Member]
+
+  def join(name:String) = {
+    val m = new Member(name)
+    members += m
+    m
+  }
+}
+
+
+object main {
+  def main(args: Array[String]): Unit = {
+    val qq = new Network
+    val wx = new Network
+
+    val jack = qq.join("jack")
+    val tom = qq.join("tom")
+
+    println(jack.equals(tom))
+
+    val andy = wx.join("andy")
+
+    println(jack.equals(andy))
+  }
+}
