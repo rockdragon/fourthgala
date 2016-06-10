a440184285c22a7f92d40d058273198db7684235
Locke <moyerock@gmail.com>
2016-06-10 19:44:47 +0800

    feat: Ducktype

---
+++ src/main/scala/ExceptionHandler.scala
@@ -0,0 +1,25 @@
+package me.moye.practise.exceptionHandler
+
+object main {
+  type Closable = {def close(): Unit}
+  def executor(target: Closable, handler: Closable => Unit): Unit = {
+    try{
+      handler(target)
+    } finally {
+      target.close()
+    }
+  }
+
+  class Affiar(val name:String="") {
+    def close(): Unit = println(name + " was closed.")
+    override def toString = "(Affiar): " + name
+  }
+
+  def Handle(o:Closable):Unit = {
+    println(o)
+  }
+
+  def main(args: Array[String]):Unit = {
+    executor(new Affiar("Jack"), Handle)
+  }
+}
