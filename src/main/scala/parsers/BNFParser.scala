168dd5a903a3ab4a7a61e42047e8ce972fe35d9f
Locke <moyerock@gmail.com>
2016-06-12 23:09:27 +0800

    feat: Parser

---
+++ src/main/scala/parsers/BNFParser.scala
@@ -0,0 +1,39 @@
+package me.moye.practise.parsers.bnfParser
+
+import scala.util.parsing.combinator._
+
+class ExprParser extends RegexParsers {
+  def number: Parser[Double] =
+    """\d+(\.\d*)?""".r ^^ {
+      _.toDouble
+    }
+
+  def factor: Parser[Double] = number | "(" ~> expr <~ ")"
+
+  def term: Parser[Double] = factor ~ rep("*" ~ factor | "/" ~ factor) ^^ {
+    case number ~ list => (number /: list) {
+      case (x, "*" ~ y) => x * y
+      case (x, "/" ~ y) => x / y
+    }
+  }
+
+  def expr: Parser[Double] = term ~ rep("+" ~ log(term)("Plus term") | "-" ~ log(term)("Minus term")) ^^ {
+    case number ~ list => (number /: list) {
+      case (x, "+" ~ y) => x + y
+      case (x, "-" ~ y) => x - y
+    }
+  }
+
+  def apply(input: String): Double = parseAll(expr, input) match {
+    case Success(result, _) => result
+    case failure : NoSuccess => scala.sys.error(failure.msg)
+  }
+}
+
+object main {
+  def main(args: Array[String]): Unit = {
+    val parser = new ExprParser()
+    parser.apply("1+2*3")
+  }
+}
+
