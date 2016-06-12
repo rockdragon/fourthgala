67d383c387d0cf744bea5a0669c5704451420344
Locke <moyerock@gmail.com>
2016-06-12 21:55:06 +0800

    feat: selftype

--- src/main/scala/SelfType.scala
+++ src/main/scala/SelfType.scala
@@ -3,17 +3,33 @@ package me.moye.practise.selfType
 abstract class Dim[T](val value: Double, val name: String) {
   this: T =>
   protected def create(v: Double): T
+
   def +(other: Dim[T]) = create(value + other.value)
+
   override def toString = value + " " + name
 }
 
 class Seconds(v: Double) extends Dim[Seconds](v, "s") {
-  override def create(v:Double) = new Seconds(v)
-}
-class Meters(v: Double) extends Dim[Meters](v, "m") {
-  override def create(v:Double) = new Meters(v)
+  override def create(v: Double) = new Seconds(v)
 }
 
+class Meters(v: Double) extends Dim[Meters](v, "m") {
+  override def create(v: Double) = new Meters(v)
+}
+
+trait A {
+  def sing() = "from a"
+}
+
+trait C {
+  this: A =>
+  val w = sing + " from c"
+}
+
+trait B {
+  this: C =>
+  val k = w
+}
 
 object main {
   def main(args: Array[String]): Unit = {
@@ -21,7 +37,11 @@ object main {
     val m2 = new Meters(300)
 
     println(m + m2)
-//    val s = new Seconds(200)
-//    println(m + s)
+    //    val s = new Seconds(200)
+    //    println(m + s)
+
+    //val b = new A with C with B
+    val b = new B with C with A
+    println(b.k)
   }
 }
