b606206021d0af85291bcdd40862659044ce188a
Locke <moyerock@gmail.com>
2016-06-19 22:51:02 +0800

    fix: boundary check

--- src/main/scala/algorithms/Rank.scala
+++ src/main/scala/algorithms/Rank.scala
@@ -8,17 +8,17 @@ object dichotomy extends App {
   }
 
   def binary(key:Int, low: Int, high: Int, arr: Seq[Int]): Int = {
-    if (low > high) -1
+    if (low > high) return -1
 
+    val mid = low + (high - low) / 2
+    if(mid > arr.length - 1) return -1
     count += 1
-
-    val mid = (low + high) / 2
-    if (key < arr(mid)) return binary(key, low, mid - 1, arr)
-    else if(key > arr(mid)) return binary(key, mid +1, high, arr)
+    if (key < arr(mid)) binary(key, low, mid - 1, arr)
+    else if(key > arr(mid)) binary(key, mid + 1, high, arr)
     else mid
   }
 
 
-  binary(66, Array(1 to 100: _*))
+  binary(111, Array(1 to 100: _*))
   println("times: ", count)
 }
