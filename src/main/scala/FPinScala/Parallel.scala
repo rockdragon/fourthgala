ca1ef38649beee651e96f988618ad31fc3a39947
Locke <moyerock@gmail.com>
2016-07-09 02:01:02 +0800

    feat: Parallel[A]

---
+++ src/main/scala/FPinScala/Parallel.scala
@@ -0,0 +1,28 @@
+package FPinScala
+
+import java.util.concurrent._
+
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      def call(): A = a(es).get
+    })
+}
