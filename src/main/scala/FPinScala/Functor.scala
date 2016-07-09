34faf50421d074b8790a1597bd30fb3b5c09064a
Locke <moyerock@gmail.com>
2016-07-09 17:41:49 +0800

    feat: Functor[F[_]]

--- src/main/scala/FPinScala/Functor.scala
+++ src/main/scala/FPinScala/Functor.scala
@@ -1,12 +1,19 @@
 package FPinScala
 
-object f extends App {
+object _f extends App {
 
   trait Functor[F[_]] {
-    def map[A, B](a: F[A])(f: A => B): F[B]
+
+    def map[A, B](fa: F[A])(f: A => B): F[B]
+
+    // unzip
+    def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) =
+      (map(fab)(_._1), map(fab)(_._2))
+
+    def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
+      case Left(fa) => map(fa)(Left(_))
+      case Right(fa) => map(fa)(Right(_))
+    }
   }
 
-  def listFunctor = new Functor[List] {
-    def map[A, B](a: List[A])(f: (A) => B) = a.map(f)
-  }
 }
