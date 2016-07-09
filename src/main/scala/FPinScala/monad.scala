4a764b3ff2e19f8587df641e64714aa0a801812b
Locke <moyerock@gmail.com>
2016-07-09 18:01:28 +0800

    feat: minimal monad trait

--- src/main/scala/FPinScala/monad.scala
+++ src/main/scala/FPinScala/monad.scala
@@ -4,6 +4,8 @@ object _m extends App {
 
   trait Monad[F[_]] {
 
+    def unit[A](a: => A): F[A]
+
     def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 
     def map[A, B](fa: F[A])(f: A => B): F[B]
