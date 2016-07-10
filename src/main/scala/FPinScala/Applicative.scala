7b63edf559a221a2aeada0b78a1c9d55de3941bb
Locke <moyerock@gmail.com>
2016-07-10 22:52:32 +0800

    feat: map3 & map4

--- src/main/scala/FPinScala/Applicative.scala
+++ src/main/scala/FPinScala/Applicative.scala
@@ -5,7 +5,8 @@ object _a extends App {
   trait Applicative[F[_]] extends _f.Functor[F] {
     // primitive combinators
     def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
-    def unit[A](a: A): F[A]
+
+    def unit[A](a: => A): F[A]
 
     // derived combinators
     def map[A, B](fa: F[A])(f: A => B): F[B] =
@@ -22,6 +23,19 @@ object _a extends App {
 
     def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
       map2(fa, fb)((a, b) => (a, b))
+
+    // F[A=>B] -> F[A] => F[B]
+    def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] =
+      map2(fab, fa)((ab, a) => ab(a))
+
+    def _map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+      apply(map(fa)(f.curried))(fb)
+
+    def map3[A, B, C, D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] =
+      apply(apply(map(fa)(f.curried))(fb))(fc)
+
+    def map4[A, B, C, D, E](fa: F[A], fb: F[B], fc: F[C], fd: F[C])(f: (A, B, C, D) => E): F[E] =
+      apply(apply(apply(map(fa)(f.curried))(fb))(fc))(fd)
   }
 
 }
