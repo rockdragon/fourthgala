101b2bbdfeb5d1d95fdfb97262dcfb5f87876c2b
Locke <moyerock@gmail.com>
2016-07-30 00:59:23 +0800

    feat: RingBuffer

--- src/test/scala/algorithms/RingBufferSpec.scala
+++ src/test/scala/algorithms/RingBufferSpec.scala
@@ -2,26 +2,29 @@ package algorithms
 
 import org.scalatest._
 
-class RingBufferSpec extends FlatSpec with Matchers {
+class RingBufferSpec extends FunSpec with Matchers {
 
-  "A RingBuffer" should "be a circular queue" in {
+  describe("A RingBuffer") {
     val ringBuffer = new RingBuffer[Int](3)
 
-    var size = ringBuffer.enqueue(1)
-    size should be (1)
-    size = ringBuffer.enqueue(2)
-    size should be (2)
-    size = ringBuffer.enqueue(3)
-    size should be (3)
-    size = ringBuffer.enqueue(4)
-    size should be (3)
+    it("should be fixed-size") {
+      var size = ringBuffer.enqueue(1)
+      size should be (1)
+      size = ringBuffer.enqueue(2)
+      size should be (2)
+      size = ringBuffer.enqueue(3)
+      size should be (3)
+      size = ringBuffer.enqueue(4)
+      size should be (3)
+    }
 
-    var elem = ringBuffer.dequeue
-    elem should be (2)
-    elem = ringBuffer.dequeue
-    elem should be (3)
-    elem = ringBuffer.dequeue
-    elem should be (4)
+    it("should be a circular queue") {
+      var elem = ringBuffer.dequeue
+      elem should be (2)
+      elem = ringBuffer.dequeue
+      elem should be (3)
+      elem = ringBuffer.dequeue
+      elem should be (4)
+    }
   }
-
 }
