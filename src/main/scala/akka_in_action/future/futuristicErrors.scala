9880c6ccd5973aa4ff8951150677ea694449c48b
Locke <moyerock@gmail.com>
2016-07-31 00:13:18 +0800

    feat: futuristic error

---
+++ src/main/scala/akka_in_action/future/futuristicErrors.scala
@@ -0,0 +1,25 @@
+package akka_in_action.future
+
+import akka.actor.ActorSystem
+
+import scala.concurrent._
+import scala.util.{Success, Failure}
+import scala.util.control.NonFatal
+
+object futuristicErrors extends App {
+  implicit val system = ActorSystem()
+  import system.dispatcher
+
+  val futureFail = future {
+    throw new Exception("Error!")
+  }
+
+  futureFail onComplete {
+    case Failure(e) => println(e)
+    case Success(value) => println(value)
+  }
+
+  futureFail andThen {
+    case _ => system.shutdown
+  }
+}
