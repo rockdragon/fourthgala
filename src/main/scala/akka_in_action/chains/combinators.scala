2e57330da650f071b55fb649a4bea7171639f5c2
Locke <moyerock@gmail.com>
2016-08-01 23:23:04 +0800

    feat: combinate future

---
+++ src/main/scala/akka_in_action/chains/combinators.scala
@@ -0,0 +1,32 @@
+package akka_in_action.chains
+
+import akka.actor.ActorSystem
+
+import scala.concurrent.Future
+
+object combinators extends App {
+  implicit val system = ActorSystem()
+  import system.dispatcher
+
+  val futureX = Future(6/0) recover {
+    case _ => 0
+  }
+  val futureY = Future(10/0) recover {
+    case _ => 0
+  }
+
+  val futures = Seq(futureX, futureY)
+
+  val fastestResponse = Future.firstCompletedOf(futures)
+
+  fastestResponse map { resp =>
+    println(resp)
+    Future(resp)
+  }
+
+  fastestResponse andThen {
+    case resp =>
+      println(resp)
+      system.shutdown
+  }
+}
