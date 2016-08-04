6107988f4cb005a7964373c97cd963619bea11a8
Locke <moyerock@gmail.com>
2016-08-05 00:09:16 +0800

    feat: 2 young 2 simple, sometimes conversations

---
+++ src/main/scala/GoTicks/com/backend.scala
@@ -0,0 +1,23 @@
+package GoTicks.com
+
+import akka.actor._
+import com.typesafe.config._
+
+object backend extends App {
+  val config = ConfigFactory.load()
+  val backend = ActorSystem("backend", config.getConfig("remote-backend"))
+
+  val path2Young = "akka.tcp://frontend@0.0.0.0:2552/user/tooYoung"
+
+  class Simple extends Actor {
+    def receive = {
+      case m => {
+        println(s"Simple received ${m}")
+        val tooYoung = backend.actorSelection(path2Young)
+        tooYoung ! "Howdy, idiot!"
+      }
+    }
+  }
+
+  backend.actorOf(Props[Simple], "tooSimple")
+}
---
+++ src/main/scala/GoTicks/com/frontend.scala
@@ -0,0 +1,22 @@
+package GoTicks.com
+
+import akka.actor.{Props, Actor, ActorSystem}
+import com.typesafe.config.ConfigFactory
+
+object frontend extends App{
+  val config = ConfigFactory.load()
+  val frontend = ActorSystem("frontend", config.getConfig("remote-frontend"))
+
+  val path2Simple = "akka.tcp://backend@0.0.0.0:2551/user/tooSimple"
+
+  class Young extends Actor {
+    def receive = {
+      case m => println(s"Young received ${m}")
+    }
+  }
+
+  frontend.actorOf(Props[Young], "tooYoung")
+
+  val simple  = frontend.actorSelection(path2Simple)
+  simple ! "hello, 2 Simple"
+}
