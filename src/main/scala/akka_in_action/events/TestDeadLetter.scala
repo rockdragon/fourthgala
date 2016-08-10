35f70787eaed355e0417fb0493324271dedbf70f
Locke <moyerock@gmail.com>
2016-08-11 00:08:24 +0800

    feat: monitor deadLetter

---
+++ src/main/scala/akka_in_action/events/TestDeadLetter.scala
@@ -0,0 +1,24 @@
+package akka_in_action.events
+
+import akka.actor.{ActorSystem, DeadLetter, PoisonPill, Props}
+import akka.testkit.TestActors.EchoActor
+
+object TestDeadLetter extends App {
+  implicit val system = ActorSystem("actor-system")
+  import system.dispatcher
+
+  val deadLetterMonitor = system.actorOf(Props(new TestActor("monitor")))
+
+  system.eventStream.subscribe(
+    deadLetterMonitor,
+    classOf[DeadLetter]
+  )
+
+  val actor = system.actorOf(Props[EchoActor], "echo")
+  actor ! PoisonPill
+
+  val msg = new Order("me", "Akka in Action", 1)
+  actor ! msg
+
+  system.shutdown
+}
