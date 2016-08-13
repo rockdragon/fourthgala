806d5c573a30faaaf7511814fb719b5c53d0cf4d
Locke <moyerock@gmail.com>
2016-08-13 23:23:20 +0800

    feat: dynamic configure hostname & port

--- src/main/scala/akka_in_action/cluster/ClusterSystem.scala
+++ src/main/scala/akka_in_action/cluster/ClusterSystem.scala
@@ -1,18 +1,32 @@
 package akka_in_action.cluster
 
 import akka.actor.ActorSystem
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 
-object ClusterSystem extends App{
-  def startSeedNode(configName: String): Unit = {
-    val seedConfig = ConfigFactory.load(configName)
+object ClusterSystem extends App {
+  def loadCommonConfig: Config = ConfigFactory.load("cluster")
+
+  def remoteConfig(hostname: String, port: Int, commonConfig: Config): Config = {
+    ConfigFactory.invalidateCaches()
+    val remoteConfigString =
+      s"""
+         akka.remote.netty.tcp.hostname = $hostname
+         akka.remote.netty.tcp.port = $port
+       """.stripMargin
+    ConfigFactory.parseString(remoteConfigString).withFallback(commonConfig)
+  }
+
+  def startSeedNode(hostname: String, port: Int, commonConfig: Config): Unit = {
+    val seedConfig = remoteConfig(hostname, port, commonConfig)
     val seedSystem = ActorSystem("words", seedConfig)
   }
 
-  startSeedNode("seed1")
+  val commonConfig = loadCommonConfig
+
+  startSeedNode("127.0.0.1", 2551, commonConfig)
 
   Thread.sleep(1000)
 
-  startSeedNode("seed2")
-  startSeedNode("seed3")
+  startSeedNode("127.0.0.1", 2552, commonConfig)
+  startSeedNode("127.0.0.1", 2553, commonConfig)
 }
