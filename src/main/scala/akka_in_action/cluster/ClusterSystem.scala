fce5c797e649e9dbd4a57e7c721a901b69dc8445
Locke <moyerock@gmail.com>
2016-08-13 23:42:36 +0800

    feat: node leaving

--- src/main/scala/akka_in_action/cluster/ClusterSystem.scala
+++ src/main/scala/akka_in_action/cluster/ClusterSystem.scala
@@ -1,6 +1,7 @@
 package akka_in_action.cluster
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Address}
+import akka.cluster.Cluster
 import com.typesafe.config.{Config, ConfigFactory}
 
 object ClusterSystem extends App {
@@ -16,17 +17,28 @@ object ClusterSystem extends App {
     ConfigFactory.parseString(remoteConfigString).withFallback(commonConfig)
   }
 
-  def startSeedNode(hostname: String, port: Int, commonConfig: Config): Unit = {
+  def startSeedNode(hostname: String, port: Int, commonConfig: Config): ActorSystem = {
     val seedConfig = remoteConfig(hostname, port, commonConfig)
-    val seedSystem = ActorSystem("words", seedConfig)
+    ActorSystem("words", seedConfig)
+  }
+
+  def leaveCluster(system: ActorSystem): Unit = {
+    val cluster = Cluster(system)
+    cluster.leave(cluster.selfAddress)
   }
 
   val commonConfig = loadCommonConfig
 
-  startSeedNode("127.0.0.1", 2551, commonConfig)
+  val system2551 = startSeedNode("127.0.0.1", 2551, commonConfig)
 
   Thread.sleep(1000)
 
-  startSeedNode("127.0.0.1", 2552, commonConfig)
-  startSeedNode("127.0.0.1", 2553, commonConfig)
+  val system2552 = startSeedNode("127.0.0.1", 2552, commonConfig)
+  val system2553 = startSeedNode("127.0.0.1", 2553, commonConfig)
+
+  Thread.sleep(10000)
+
+  1 to 6 foreach { _ => println("[DONE] ------------------->>> ") }
+
+  leaveCluster(system2551)
 }
