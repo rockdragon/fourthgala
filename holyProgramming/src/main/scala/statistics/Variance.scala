58b6506a63714f696ce1b8e921d15344ceab3e87
Locke <moyerock@gmail.com>
2016-08-23 00:30:27 +0800

    feat: Variance computation

--- holyProgramming/src/main/scala/statistics/Variance.scala
+++ holyProgramming/src/main/scala/statistics/Variance.scala
@@ -3,7 +3,7 @@ package statistics
 
 import scala.math._
 
-object variance extends App {
+object Variance extends App {
   val seqs = List[(Int, Int)](145 -> 4, 155 -> 9, 165 -> 16, 175 -> 27,
     185 -> 20, 195 -> 17, 205 -> 10, 215 -> 8, 225 -> 4, 235 -> 5)
 
