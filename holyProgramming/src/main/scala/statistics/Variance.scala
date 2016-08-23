04db25cea17ade6f79e16ac23cbda38195daebce
Locke <moyerock@gmail.com>
2016-08-23 21:42:55 +0800

    feat: Variance

--- holyProgramming/src/main/scala/statistics/Variance.scala
+++ holyProgramming/src/main/scala/statistics/Variance.scala
@@ -4,14 +4,31 @@ package statistics
 import scala.math._
 
 object Variance extends App {
-  val seqs = List[(Int, Int)](145 -> 4, 155 -> 9, 165 -> 16, 175 -> 27,
-    185 -> 20, 195 -> 17, 205 -> 10, 215 -> 8, 225 -> 4, 235 -> 5)
+  def groupedVariance(): Unit = {
+    val seqs = List[(Int, Int)](145 -> 4, 155 -> 9, 165 -> 16, 175 -> 27,
+      185 -> 20, 195 -> 17, 205 -> 10, 215 -> 8, 225 -> 4, 235 -> 5)
 
-  val x_ = 185
-  val n_1 = 120
+    val x_ = 185
+    val n_1 = 120
 
-  val variance = seqs.foldRight(0.0)((p, acc) =>
-    pow((p._1 - x_), 2) * p._2 + acc
-  )
-  println(variance / n_1)
+    val variance = seqs.foldRight(0.0)((p, acc) =>
+      pow((p._1 - x_), 2) * p._2 + acc
+    ) / n_1
+    println(variance)
+    println(sqrt(variance))
+  }
+
+  def ungroupedVariance(): Unit = {
+    val seqs = List[Int](145, 155, 165, 175, 185, 195, 205, 215, 225, 235)
+    val n = seqs.length
+    val x_ = seqs.sum / n
+
+    val variance = seqs.foldRight(0.0)((c, acc) =>
+      pow(c - x_, 2) + acc) / (n - 1)
+    println(variance)
+    println(sqrt(variance))
+  }
+
+  groupedVariance()
+  ungroupedVariance()
 }
