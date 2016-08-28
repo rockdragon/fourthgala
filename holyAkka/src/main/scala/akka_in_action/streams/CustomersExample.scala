575d08405a50e872b4614abc98350189c4dffb25
Locke <moyerock@gmail.com>
2016-08-29 00:22:25 +0800

    feat: linear streaming

--- holyAkka/src/main/scala/akka_in_action/streams/CustomersExample.scala
+++ holyAkka/src/main/scala/akka_in_action/streams/CustomersExample.scala
@@ -7,25 +7,32 @@ import akka.stream.scaladsl.{Flow, Sink, Source}
 import scala.util.Random
 
 object InputCustomer {
-  def random():InputCustomer = {
-    InputCustomer(s"FirstName${Random.nextInt(1000)} LastName${Random.nextInt(1000)}")
+  def random(): InputCustomer = {
+    InputCustomer(s"FirstName${Random.nextInt(1000)} " +
+      s"LastName${Random.nextInt(1000)}")
   }
 }
+
 case class InputCustomer(name: String)
+
 case class OutputCustomer(firstName: String, lastName: String)
 
 object CustomersExample extends App {
   implicit val actorSystem = ActorSystem()
+
   import actorSystem.dispatcher
+
   implicit val materializer = ActorMaterializer()
 
   val inputCustomers = Source((1 to 100).map(_ => InputCustomer.random()))
 
-  val normalize = Flow[InputCustomer].map(c => c.name.split(" ").toList).collect {
-    case firstName::lastName::Nil => OutputCustomer(firstName, lastName)
-  }
+  val normalize = Flow[InputCustomer]
+    .map(c => c.name.split(" ").toList)
+    .collect {
+      case firstName :: lastName :: Nil => OutputCustomer(firstName, lastName)
+    }
 
-  val writeCustomers = Sink.foreach[OutputCustomer] {println} // eta-conversion
+  val writeCustomers = Sink.foreach { println} // η-conversion
 
   inputCustomers.via(normalize).runWith(writeCustomers).andThen {
     case _ => actorSystem.terminate()
