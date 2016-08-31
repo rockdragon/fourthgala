b689f3703abea5663a046a4396e313b31bdbc9fd
Locke <moyerock@gmail.com>
2016-09-01 00:20:08 +0800

    chore: triangle graph

--- holyAkka/src/main/scala/akka_in_action/streams/GraphBuilder.scala
+++ holyAkka/src/main/scala/akka_in_action/streams/GraphBuilder.scala
@@ -13,24 +13,28 @@ object GraphBuilder extends App {
   implicit val materializer = ActorMaterializer()
 
   val topHeadSink = Sink.head[Int]
+  val middleSink = Sink.head[Int]
   val bottomHeadSink = Sink.head[Int]
   val sharedDoubler = Flow[Int].map(_ * 2)
 
-  val results = RunnableGraph.fromGraph(GraphDSL.create(topHeadSink, bottomHeadSink)((_, _)) { implicit builder =>
-    (topHS, bottomHS) =>
+  val results = RunnableGraph.fromGraph(GraphDSL.create(topHeadSink, middleSink, bottomHeadSink)
+  ((_, _, _)) { implicit builder =>
+    (topHS, midHS, bottomHS) =>
       import GraphDSL.Implicits._
-      val broadcast = builder.add(Broadcast[Int](2))
-      Source.fromIterator(() => Iterator from 2) ~> broadcast.in
+      val broadcast = builder.add(Broadcast[Int](3))
+      Source.repeat(3) ~> broadcast.in
 
       broadcast.out(0) ~> sharedDoubler ~> topHS.in
-      broadcast.out(1) ~> sharedDoubler ~> bottomHS.in
+      broadcast.out(1) ~> sharedDoubler ~> midHS.in
+      broadcast.out(2) ~> sharedDoubler ~> bottomHS.in
 
       ClosedShape
   }).run()
 
   val r1 = Await.result(results._1, 300 millis)
   val r2 = Await.result(results._2, 300 millis)
+  val r3 = Await.result(results._3, 300 millis)
 
-  println(r1, r2)
+  println(r1, r2, r3)
   system.terminate()
 }
