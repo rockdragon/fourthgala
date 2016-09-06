e4abc0447b665f240139846160dc60d22102c50f
Locke <moyerock@gmail.com>
2016-09-06 21:46:33 +0800

    feat: holy runnableGraph with GraphDSL

--- holyAkka/src/main/scala/akka_in_action/streams/GraphBuilder.scala
+++ holyAkka/src/main/scala/akka_in_action/streams/GraphBuilder.scala
@@ -3,7 +3,7 @@ package akka_in_action.streams
 
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, ClosedShape}
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{Balance, Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -14,19 +14,20 @@ object GraphBuilder extends App {
 
   val topHeadSink = Sink.head[Int]
   val middleSink = Sink.head[Int]
-  val bottomHeadSink = Sink.head[Int]
+  val bottomHeadSink = Sink.seq[Int]
   val sharedDoubler = Flow[Int].map(_ * 2)
 
   val results = RunnableGraph.fromGraph(GraphDSL.create(topHeadSink, middleSink, bottomHeadSink)
   ((_, _, _)) { implicit builder =>
     (topHS, midHS, bottomHS) =>
       import GraphDSL.Implicits._
-      val broadcast = builder.add(Broadcast[Int](3))
-      Source.repeat(3) ~> broadcast.in
+      val broadcast = builder.add(Broadcast[Int](2))
+      val source = builder.add(Source(1 to 10))
 
+      Source.repeat(2) ~> broadcast.in
       broadcast.out(0) ~> sharedDoubler ~> topHS.in
       broadcast.out(1) ~> sharedDoubler ~> midHS.in
-      broadcast.out(2) ~> sharedDoubler ~> bottomHS.in
+      source ~> bottomHS.in
 
       ClosedShape
   }).run()
