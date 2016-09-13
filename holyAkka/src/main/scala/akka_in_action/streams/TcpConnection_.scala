7561870561fabd8b24b09cb2bcb6ee4a4d09f3ff
Locke <moyerock@gmail.com>
2016-09-13 22:07:36 +0800

    feat: TCP connection based on Message

---
+++ holyAkka/src/main/scala/akka_in_action/streams/TcpConnection_.scala
@@ -0,0 +1,48 @@
+
+package akka_in_action.streams
+
+import akka.actor.{ Actor, ActorRef, Props }
+import akka.io.{ IO, Tcp }
+import akka.util.ByteString
+import java.net.InetSocketAddress
+
+object TcpConnection_ extends App {
+
+  object Client {
+    def props(remote: InetSocketAddress, replies: ActorRef) =
+      Props(classOf[Client], remote, replies)
+  }
+
+  class Client(remote: InetSocketAddress, listener: ActorRef) extends Actor {
+
+    import Tcp._
+    import context.system
+
+    IO(Tcp) ! Connect(remote)
+
+    def receive: Receive = {
+      case CommandFailed(_: Connect) =>
+        listener ! "connect failed"
+        context stop self
+
+      case c @ Connected(remote, local) =>
+        listener ! c
+        val connection = sender()
+        connection ! Register(self)
+        context become {
+          case data: ByteString =>
+            connection ! Write(data)
+          case CommandFailed(w: Write) =>
+            // O/S buffer was full
+            listener ! "write failed"
+          case Received(data) =>
+            listener ! data
+          case "close" =>
+            connection ! Close
+          case _: ConnectionClosed =>
+            listener ! "connection closed"
+            context stop self
+        }
+    }
+  }
+}
