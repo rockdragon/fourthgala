65590ace6d503bda810338192ed02259312ad846
Locke <moyerock@gmail.com>
2016-09-14 18:25:02 +0800

    feat: placeholder for SimpleEchoHandler

---
+++ holyAkka/src/main/scala/akka_in_action/streams/TCP/SimpleEchoHandler_.scala
@@ -0,0 +1,12 @@
+
+package akka_in_action.streams.TCP
+
+import java.net.InetSocketAddress
+
+import akka.actor.{ActorLogging, Actor, ActorRef, Props}
+import akka.io.{IO, Tcp}
+import akka.util.ByteString
+
+object SimpleEchoHandler_ {
+
+}
